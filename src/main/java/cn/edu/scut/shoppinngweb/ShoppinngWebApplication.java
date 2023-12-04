5b8a8f8e12d41d24e6533ec8dc0e819fb590d011
ZzZ <1317013545@qq.com>
2023-12-04 15:21:05 +0800

    init

---
+++ src/main/java/cn/edu/scut/shoppinngweb/ShoppinngWebApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.scut.shoppinngweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppinngWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppinngWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/scut/shoppinngweb/ShoppinngWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.scut.shoppinngweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinngWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

df4afc55c7adcf853277689f69467e0bbc30fcfe
ZzZ <1317013545@qq.com>
2023-12-07 10:55:10 +0800

    update

--- src/main/java/cn/edu/scut/shoppinngweb/controller/CartController.java
+++ src/main/java/cn/edu/scut/shoppinngweb/controller/CartController.java
@@ -1,14 +1,14 @@
 package cn.edu.scut.shoppinngweb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/cart")
+@Controller
 public class CartController {
-    @GetMapping()
-    public String login(){
+    @RequestMapping("/cart")
+    public String cart(){
         return "cart.html";
     }
 }
--- src/main/java/cn/edu/scut/shoppinngweb/controller/HomeController.java
+++ src/main/java/cn/edu/scut/shoppinngweb/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 
 public class HomeController {
-    @RequestMapping(value = "/home")
+    @RequestMapping("/home")
     public String home(){
         return "home.html";
     }
